9303a8e9f0eb74b01fdadfb1c9537a9eb7680ad6
Mehmet Ipek <mhmtipek@gmail.com>
2020-02-16 17:48:51 +0100

    fix path for layers and pass .vimrc to image

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-docker run -it --volume $PWD/Playground:/opt/yocto/Playground --volume $PWD/poky:/opt/yocto/poky yocto-playground
+docker run -it --volume $PWD/layers:/opt/yocto/layers --volume $HOME/.vimrc:/home/build/.vimrc --volume $PWD/poky:/opt/yocto/poky yocto-playground
+
