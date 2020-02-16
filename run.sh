35b77f820f3e1ffe90e65fb5e9d42a3c342aea31
Mehmet Ipek <mhmtipek@gmail.com>
2020-02-17 00:23:07 +0100

    include build directory also

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -it --volume $PWD/layers:/opt/yocto/layers --volume $HOME/.vimrc:/home/build/.vimrc --volume $PWD/poky:/opt/yocto/poky yocto-playground
+docker run -it --volume $PWD/layers:/opt/yocto/layers --volume $PWD/build:/opt/yocto/build --volume $HOME/.vimrc:/home/build/.vimrc --volume $PWD/poky:/opt/yocto/poky yocto-playground
 
